8dc91ba5b2a4296e802e37c28a3154b825f908d0
Felix-bongue <felixbongue@outlook.com>
2025-04-27 18:42:41 -0700

    Create PrivateRoute.tsx

---
+++ frontend/src/components/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+    import { Navigate } from 'react-router-dom'
+    import { useAuth } from '../hooks/useAuth'
+
+    interface PrivateRouteProps {
+      children: React.ReactNode
+    }
+
+    const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+      const { user, loading } = useAuth()
+
+      if (loading) {
+        return <div>Loading...</div>
+      }
+
+      if (!user) {
+        return <Navigate to="/" replace />
+      }
+
+      return <>{children}</>
+    }
+
+    export default PrivateRoute
